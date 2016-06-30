cf74255df38cc8f78deb9a3ec03c45caa0d8219c
Ilya <mordkovich.ilya@googlemail.com>
2016-06-30 22:08:09 +0200

    starter pack with running grunt

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      my_target: {
+        files: {
+          'public/all.min.js': ['node_modules/jquery/dist/jquery.js', 'node_modules/bootstrap/dist/bootstrap.js', 'my.js']
+        }
+      }
+    },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'public/all.min.css': ['node_modules/bootstrap/dist/css/bootstrap.css', 'my.css']
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['my.css', 'my.js'],
+        tasks: ['default'],
+        options: {
+          spawn: false,
+        },
+      },
+    }
+  });
+
+  // Run
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+  // Default tasks
+  grunt.registerTask('default', ['uglify', 'cssmin', 'watch']);
+
+};
---
+++ my.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    MyApp = {
+        appinit: function() {
+            MyApp.DocSidebarToggle();
+            //more
+        },
+
+
+        DocSidebarToggle: function() {
+            console.log('grunting is cool. like it');
+        }
+    };
+
+    MyApp.appinit();
+});
